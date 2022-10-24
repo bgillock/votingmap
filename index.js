303f754232b69b75aa3a83cd1f9bfde2bbc655ab
Bill Gillock <bitrate27@gmail.com>
2022-10-24 10:01:23 -0500

    Harris county courthouse marker

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 // Initialize and add the map
+var geocoder;
+var map;
+function addMarker(name,address) {
+    geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == 'OK') {
+          map.setCenter(results[0].geometry.location);
+          var marker = new google.maps.Marker({
+              title: name,
+              map: map,
+              position: results[0].geometry.location
+          });
+        } else {
+          alert('Geocode was not successful for the following reason: ' + status);
+        }
+      });
+}
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.031 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
+    geocoder = new google.maps.Geocoder();
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 8,
+        center: houston,
+      });
+
+    // The location of Houston
+    addMarker("Harris County Courthouse","301 Fannin St. Houston, TX 77002");
   }
   
   window.initMap = initMap;
