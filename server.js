872ee0d6123f4234f1b36b696b7d2388fa6a77f5
Bill Gillock <bitrate27@gmail.com>
2022-10-24 09:30:02 -0500

    Serve directory

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ const port = process.env.PORT || 8080;
 app.get('/index.html', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
-
+app.use(express.static(__dirname));
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
